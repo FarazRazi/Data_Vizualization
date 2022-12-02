6dd9ff9abab4d8c39b50ba13a23b7eee0a14d179
FarazRazi <72218210+FarazRazi@users.noreply.github.com>
2022-12-02 14:57:03 +0500

    added scale time - group by species / colors

--- Project/bundles/main.js
+++ Project/bundles/main.js
@@ -1,7 +1,16 @@
 // import _data from "./birdstrikes.json" assert { type: "json" };
 import { scatterPlot } from "./scatterPlot.js";
 d3.csv("./birdstrikes.csv").then((_data) => {
-  console.log("Data Added", _data);
-  console.log("flight Data", _data[0]["Flight Date"]);
-  scatterPlot(_data, 680, 360);
+  // console.log("Data Added", _data);
+  let groupBy = "months";
+  // console.log("flight Data", _data[0]["Flight Date"]);
+  scatterPlot(_data, 680, 360, groupBy);
+  const toggleTime = () => {
+    if (groupBy === "months") groupBy = "years";
+    else groupBy = "months";
+    scatterPlot(_data, 680, 360, groupBy);
+  };
+  // toggleTime();
+  let x = document.getElementById("demo");
+  x.addEventListener("click", toggleTime);
 });
--- Project/bundles/scatterPlot.js
+++ Project/bundles/scatterPlot.js
@@ -1,5 +1,6 @@
 export const scatterPlot = (data, widthProp, heightProp, groupBy) => {
-  var margin = { top: 10, right: 30, bottom: 90, left: 40 },
+  d3.select(".barChart > *").remove();
+  var margin = { top: 40, right: 30, bottom: 40, left: 40 },
     width = widthProp - margin.left - margin.right,
     height = heightProp - margin.top - margin.bottom;
   var svg = d3
@@ -9,12 +10,29 @@ export const scatterPlot = (data, widthProp, heightProp, groupBy) => {
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+  console.log(groupBy);
+  let species = new Set();
   //   Parse the Data
   var nested_data = d3
     .nest()
     .key(function (d) {
-      return d["Flight Date"].split("-").slice(1, 2).join("-");
+      if (groupBy === "months") {
+        const date = new Date(
+          0,
+          d["Flight Date"].split("-").slice(1, 2).join("-"),
+          1
+        );
+        // console.log(date);
+        return date;
+      } else if (groupBy === "years") {
+        const date = new Date(
+          d["Flight Date"].split("-").slice(0, 1).join("-"),
+          d["Flight Date"].split("-").slice(1, 2).join("-"),
+          1
+        );
+        // console.log(date);
+        return date;
+      }
     })
     .sortKeys(d3.ascending)
     .rollup(function (leaves) {
@@ -22,36 +40,43 @@ export const scatterPlot = (data, widthProp, heightProp, groupBy) => {
         sum: d3.sum(leaves, function (d) {
           return 1;
         }),
-        state: d3.group(leaves, (d) => {
-          return d["Origin State"];
-        }),
-        specie: d3.group(leaves, (d) => {
+        // state: d3.group(leaves, (d) => {
+        //   return d["Origin State"];
+        // }),
+        species: d3.group(leaves, (d) => {
+          species.add(d["Wildlife Species"]);
           return d["Wildlife Species"];
         }),
       };
     })
     .entries(data);
-
-  var bar_chart_data = nested_data.map(function (d) {
-    return { date: d.key, sum: d.value.sum, specie: d.value.specie };
+  var bar_chart_data = [];
+  nested_data.forEach(function (d) {
+    Array.from(d.value.species, ([key, values]) => {
+      bar_chart_data.push({
+        date: d.key,
+        key: key,
+        sum: values.length,
+        values: values,
+      });
+    });
   });
   console.log(bar_chart_data);
 
   //   X axis
   var x = d3
-    .scaleBand()
-    .range([0, width])
-    .domain(bar_chart_data.map((d) => d.date))
-    .padding(0.2);
+    .scaleTime()
+    .domain([new Date(0, 0, 1), new Date(1, 0, 0)])
+    .range([0, width]);
 
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end")
-    .attr("marker-end", "url(#arrow)");
+    .call(
+      d3.axisBottom(x).tickFormat(function (date) {
+        return d3.timeFormat("%b")(date);
+      })
+    );
 
   //   svg
   //     .append("text")
@@ -72,13 +97,22 @@ export const scatterPlot = (data, widthProp, heightProp, groupBy) => {
   //     .attr("stroke", "black");
 
   // Add Y axis
-  console.log(d3.max(bar_chart_data.map((d) => d.sum)));
+  // console.log(d3.max(bar_chart_data.map((d) => d.sum)));
   var y = d3
-    .scaleLinear()
-    .domain([0, d3.max(bar_chart_data.map((d) => d.sum))])
+    .scalePow()
+    .exponent(0.1)
+    .domain([
+      d3.min(bar_chart_data.map((d) => d.sum)) - 0.2,
+      d3.max(bar_chart_data.map((d) => d.sum)),
+    ])
     .range([height, 0]);
   svg.append("g").call(d3.axisLeft(y)).attr("marker-end", "url(#arrow)");
-
+  var clrRange = ["red", "blue", "green", "yellow"];
+  var colScale = d3
+    .scaleOrdinal()
+    .domain(bar_chart_data.map((d) => d.key))
+    .range(clrRange);
+  // console.log(colScale);
   var tooltip = d3
     .select("#barChart")
     .append("div")
@@ -91,47 +125,132 @@ export const scatterPlot = (data, widthProp, heightProp, groupBy) => {
     .style("padding", "5px")
     .style("position", "absolute");
 
-  var mouseover = function () {
-    var data = d3.select(this).datum();
+  var mouseover = function (event, d) {
     tooltip
-      .html("Strikes: " + data.sum + "<br>" + "Specie: " + data.specie + "<br>")
+      .html("Strikes: " + d.sum + "<br>" + "Specie: " + d.key + "<br>")
       .style("opacity", 1);
+    d3.selectAll("circle").each(function (dAll) {
+      if (d.key === dAll.key) {
+        d3.select(this).attr("r", 10);
+      }
+    });
   };
   const mousemove = function (event, d) {
     tooltip
       .style("transform", "translateY(-33%)")
-      .style("left", event.x / 2 + "px")
-      .style("top", event.y / 2 + "px");
+      .style("left", event.x / 1.3 + "px")
+      .style("top", event.y / 1.3 + "px");
   };
   const mouseleave = function (event, d) {
-    tooltip.style("opacity", 0);
+    // console.log(event, d, this);
+    d3.selectAll("circle").each(function (dAll) {
+      if (d.key === dAll.key) {
+        d3.select(this).attr("r", 5);
+        tooltip.style("opacity", 0);
+      }
+    });
   };
+  // svg
+  //   .selectAll("mybar")
+  //   .data(bar_chart_data)
+  //   .enter()
+  //   .append("rect")
+  //   .attr("fill", "#69b3a2")
+  //   .attr("x", function (d) {
+  //     return x(d.date);
+  //   })
+  //   .attr("width", x.bandwidth())
+  //   // no bar at the beginning thus:
+  //   .attr("height", function (d) {
+  //     return height - y(0);
+  //   }) // always equal to 0
+  //   .attr("y", function (d) {
+  //     return y(0);
+  //   })
+  //   .on("mouseover", mouseover)
+  //   .on("mousemove", mousemove)
+  //   .on("mouseleave", mouseleave);
+
   var g = svg.selectAll("mybar").data(bar_chart_data).enter().append("g");
+  // .selectAll("circle");
+  // .data((d) => {
+  //   var keys = [...species.keys()];
+  //   // let array = [{}];
+  //   // d.forEach((s) => {
+  //   //   const species = s.species;
+  //   //   species.forEach((s) => {
+  //   //     array.push(s);
+  //   //   });
+  //   // });
+  //   var totals = d3
+  //     .nest()
+  //     .rollup((v) => {
+  //       var array = [];
+  //       // var result = {
+  //       //   date: v.date,
+  //       //   // sum: v.sum,
+  //       // };
+  //       // console.log(v);
+  //       Array.from(v.species, ([key, values]) => {
+  //         array.push({
+  //           date: v.date,
+  //           key: key,
+  //           sum: values.length,
+  //           values: values,
+  //         });
+  //       });
+  //       // keys.forEach((key) => {
+  //       // console.log(v.species.get(key));
+  //       // result.arr.push();
+  //       // });
+  //       // console.log(array);
+  //       return array;
+  //     })
+  //     .entries(d);
+  //   // console.log(totals);
+  //   return totals;
+  // })
+  // .enter();
   //   dots on chart
   g.append("circle") // Uses the enter().append() method
     .attr("class", "dot") // Assign a class for styling
     .attr("cx", function (d) {
-      return x(d.date) + x.bandwidth() / 2;
+      // console.log(d.date);
+      return x(new Date(d.date)) - x(new Date(0, 1, 0));
     })
     .attr("cy", function (d) {
       return y(0);
     })
     .attr("r", 0.1)
+    .attr("fill", (d) => colScale(d.key))
     .on("mouseover", mouseover)
     .on("mousemove", mousemove)
     .on("mouseleave", mouseleave);
+
   svg
     .selectAll("circle")
     .transition()
     .duration(800)
-    .attr("cx", function (d) {
-      return x(d.date) + x.bandwidth() / 2;
-    })
     .attr("cy", function (d) {
+      // console.log(d.species);
       return y(d.sum);
     })
-    .attr("r", 2)
-    .delay(function (d, i) {
-      return i * 10;
-    });
+    .attr("r", 5);
+  // .delay(function (d, i) {
+  //   return i * 10;
+  // });
+
+  // svg
+  //   .selectAll("rect")
+  //   .transition()
+  //   .duration(800)
+  //   .attr("y", function (d) {
+  //     return y(d.sum);
+  //   })
+  //   .attr("height", function (d) {
+  //     return height - y(d.sum);
+  //   })
+  //   .delay(function (d, i) {
+  //     return i * 10;
+  //   });
 };
